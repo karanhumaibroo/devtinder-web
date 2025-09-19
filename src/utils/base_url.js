44565c5a21291f71aa4b55d9c78a4a66b7684f8c
ADITYA CHOUKSEY <adityachouksey1000@gmail.com>
2025-09-19 19:49:35 +0530

    baseurldynamic

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,9 @@
-const BASE_URL="/api";
+//for production
+
+//const BASE_URL="/api";
+//export default BASE_URL;
+
+
+//for local testing
+const BASE_URL=  location.hostname==="localhost"  ? "http://localhost:8000" : "/api";
 export default BASE_URL;
